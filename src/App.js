4a1ccf5929b7d5fc1a4260fb56707876ba099436
Andrew Dillon <andrew_dillon@unigroup.com>
2019-11-03 17:15:00 -0600

    TODO list code and README

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import 'semantic-ui-css/semantic.min.css';
+
+import { Button, Input, Divider } from 'semantic-ui-react';
 
 function App() {
+  const [newTodoText, setNewTodoText] = useState('');
+
+  const [todoItems, setTodoItems] = useState([
+    { text: 'Wash dishes' },
+    { text: 'Do homework' },
+    { text: 'Fix dinner' },
+  ]);
+
+  const handleAddTodo = () => {
+    const newTodoItems = [...todoItems, { text: newTodoText }];
+    setTodoItems(newTodoItems);
+    setNewTodoText('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <div style={{ marginTop: '25px' }}>
+        <TodoInput
+          todoText={newTodoText}
+          onChangeTodoText={event => {
+            setNewTodoText(event.target.value);
+          }}
+        />
+        <AddButton onClickAdd={handleAddTodo} />
+        <TodoHeader />
+        <Divider />
+        <TodoList items={todoItems} />
+      </div>
     </div>
   );
+}
+
+function TodoHeader() {
+  return <h1>TODO List</h1>;
+}
+
+function TodoInput({ todoText, onChangeTodoText }) {
+  return (
+    <Input
+      style={{ marginRight: '15px' }}
+      placeholder="Learn React..."
+      value={todoText}
+      onChange={onChangeTodoText}
+    />
+  );
+}
+
+function AddButton({ onClickAdd }) {
+  return (
+    <Button primary={true} onClick={onClickAdd}>
+      Add
+    </Button>
+  );
+}
+
+function TodoList({ items }) {
+  return items.map(item => <div>{item.text}</div>);
 }
 
 export default App;
